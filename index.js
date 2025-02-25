ccb6bf0820c960a6cca063c3fcc4f5ecb2b9b100
Hafsa Egal <hafsaegal915@gmail.com>
2025-02-25 21:36:47 +0300

    functions lab

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 function shout(string) {
     return string.toUpperCase()
 }
+
+function whisper(string) {
+    return string.toLowerCase()
+}
+
+function logShout(hello) {
+    console.log('hello'.toUpperCase())
+
+}
+function logWhisper(HELLO) {
+    console.log('HELLO'.toLowerCase())
+}
+
+function sayHiToHeadphonedRoommate(string){
+    if (hello = toLowerCase())
+        return "I can't hear you!" 
+    if (HELLO = upperCase())
+        return "YES INDEED!"
+    if (string = "Let's have dinner together!") 
+        return "I would love to!"
+
+
+}
